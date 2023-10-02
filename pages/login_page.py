ec30aa816ef4fba09ad34c110da462155f9984b6
EnoqueQa <enoquefernandesqa@gmail.com>
2023-10-02 11:37:23 -0300

    Organizing the locators

--- pages/login_page.py
+++ pages/login_page.py
@@ -6,8 +6,11 @@ class LoginPage:
     
     def __init__(self):
         self.driver = conftest.driver
+        self.username_field = (By.ID, "user-name")
+        self.password_field = (By.ID, "password")
+        self.login_button = (By.ID, "login-button")
         
     def login(self, users, password):
-        self.driver.find_element(By.ID, "user-name").send_keys(users)
-        self.driver.find_element(By.ID, "password").send_keys(password)
-        self.driver.find_element(By.ID, "login-button").click()
+        self.driver.find_element(*self.username_field).send_keys(users)
+        self.driver.find_element(*self.password_field).send_keys(password)
+        self.driver.find_element(*self.login_button).click()
